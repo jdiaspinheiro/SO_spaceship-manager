0a84422fff3cd2d7b20a89807a8295028dccbb68
João Dias Pinheiro <NB27498@novabase.pt>
2021-06-11 17:18:13 +0100

    Initial commit

---
+++ src/main/java/com/celfocus/training/spaceover/spaceship/manager/SpaceshipManagerApplication.java
@@ -0,0 +1,13 @@
+package com.celfocus.training.spaceover.spaceship.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceshipManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceshipManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/celfocus/training/spaceover/spaceship/manager/SpaceshipManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celfocus.training.spaceover.spaceship.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceshipManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
