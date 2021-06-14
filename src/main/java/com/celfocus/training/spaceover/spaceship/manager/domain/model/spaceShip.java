5ec49e3cb4d9816666445df3777671c83f5b21e3
João Dias Pinheiro <NB27498@novabase.pt>
2021-06-14 10:06:07 +0100

    [development]:[finalized spaceship model]

--- src/main/java/com/celfocus/training/spaceover/spaceship/manager/domain/model/spaceShip.java
+++ src/main/java/com/celfocus/training/spaceover/spaceship/manager/domain/model/spaceShip.java
@@ -1,4 +1,31 @@
 package com.celfocus.training.spaceover.spaceship.manager.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class spaceShip {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String name;
+
+    @NotEmpty
+    private String status;
+
+    @NotEmpty
+    private int maxOcuppancy;
+
+    @NotEmpty
+    private String type;
 }
